7a10028c22a5820fe43c6f7ebf14430febdc0f92
Viktor Ohád <viktor.ohad@protonmail.com>
2022-12-02 15:02:31 +0100

    day 2

---
+++ day-2/src/main.rs
@@ -0,0 +1,143 @@
+use std::{cmp::Ordering, fs, io::BufRead};
+
+#[derive(Debug)]
+struct Match {
+    round: usize,
+    player_1: Weapon,
+    player_2: Weapon,
+    outcome: OutCome,
+}
+
+#[derive(Debug, Eq, PartialEq, PartialOrd, Clone, Copy)]
+enum Weapon {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum OutCome {
+    Draw,
+    Win,
+    Lose,
+}
+
+impl Ord for Weapon {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self {
+            Weapon::Rock => match other {
+                Weapon::Rock => Ordering::Equal,
+                Weapon::Paper => Ordering::Less,
+                Weapon::Scissors => Ordering::Greater,
+            },
+            Weapon::Paper => match other {
+                Weapon::Rock => Ordering::Greater,
+                Weapon::Paper => Ordering::Equal,
+                Weapon::Scissors => Ordering::Less,
+            },
+            Weapon::Scissors => match other {
+                Weapon::Rock => Ordering::Less,
+                Weapon::Paper => Ordering::Greater,
+                Weapon::Scissors => Ordering::Equal,
+            },
+        }
+    }
+}
+
+fn get_weapon(s: &str) -> Weapon {
+    match s {
+        "A" | "X" => Weapon::Rock,
+        "B" | "Y" => Weapon::Paper,
+        _ => Weapon::Scissors,
+    }
+}
+
+fn get_point(m: &Match) -> isize {
+    let weapon_points = match m.player_2 {
+        Weapon::Rock => 1,
+        Weapon::Paper => 2,
+        Weapon::Scissors => 3,
+    };
+
+    let outcome_points = match m.player_1.cmp(&m.player_2) {
+        Ordering::Less => 6,
+        Ordering::Equal => 3,
+        Ordering::Greater => 0,
+    };
+
+    weapon_points + outcome_points
+}
+
+fn read_input() -> Vec<Match> {
+    let contents = fs::read("input").expect("Not able to read the input");
+    let mut count = 1;
+    let mut matches: Vec<Match> = vec![];
+
+    for line in contents.lines() {
+        let line = line.unwrap();
+        let first = line.split(" ").nth(0).expect("Not able to parse line");
+        let second = line.split(" ").nth(1).expect("Not able to parse line");
+
+        matches.push(Match {
+            round: count,
+            player_1: get_weapon(first),
+            player_2: get_weapon(second),
+            outcome: match second {
+                "X" => OutCome::Lose,
+                "Y" => OutCome::Draw,
+                _ => OutCome::Win,
+            },
+        });
+
+        count += 1;
+    }
+
+    matches
+}
+
+fn part_1() {
+    let matches = read_input();
+    let result = matches.iter().fold(0, |mut acc, m| {
+        acc += get_point(m);
+        acc
+    });
+
+    println!("[Part 1] Result: {}", result);
+}
+
+fn part_2() {
+    let result = read_input()
+        .iter()
+        .map(|m| {
+            let p2_weapon = match m.outcome {
+                OutCome::Draw => m.player_1,
+                OutCome::Win => match m.player_1 {
+                    Weapon::Rock => Weapon::Paper,
+                    Weapon::Paper => Weapon::Scissors,
+                    Weapon::Scissors => Weapon::Rock,
+                },
+                OutCome::Lose => match m.player_1 {
+                    Weapon::Rock => Weapon::Scissors,
+                    Weapon::Paper => Weapon::Rock,
+                    Weapon::Scissors => Weapon::Paper,
+                },
+            };
+
+            Match {
+                round: m.round,
+                player_1: m.player_1,
+                player_2: p2_weapon,
+                outcome: m.outcome,
+            }
+        })
+        .fold(0, |mut acc, m| {
+            acc += get_point(&m);
+            acc
+        });
+    println!("[Part 2] Result: {}", result);
+}
+
+fn main() {
+    part_1();
+    part_2();
+}
